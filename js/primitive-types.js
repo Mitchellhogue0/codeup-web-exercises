1a1e59cba000b93065f46cc780d046c62b0f2235
Mitch Hogue <mitchhogue@Mitchs-MacBook-Pro.local>
2021-04-15 15:13:13 -0500

    pushes exercise files

---
+++ js/primitive-types.js
@@ -0,0 +1,34 @@
+"use strict";
+
+console.log(typeof (99.9));
+
+console.log(typeof ("false"));
+
+console.log(typeof (false));
+
+console.log(typeof ('0'));
+
+console.log(typeof (0));
+
+console.log(typeof (true));
+
+console.log(typeof ('true'));
+
+//A term or phrase typed into a search box?
+//string
+
+//If a user is logged in?
+//boolean
+
+//A discount amount to apply to a user's shopping cart?
+//number
+
+//Whether or not a coupon code is valid?
+//boolean
+
+//An email address typed into a registration form?
+//string
+
+//The price of a product?
+//string
+
