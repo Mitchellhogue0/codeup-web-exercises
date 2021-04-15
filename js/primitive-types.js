9d908f95b8355eba6fdcdeacd8e3517b607a9d95
Mitch Hogue <mitchhogue@Mitchs-MacBook-Pro.local>
2021-04-15 15:26:09 -0500

    updates exercise

--- js/primitive-types.js
+++ js/primitive-types.js
@@ -1,18 +1,19 @@
 "use strict";
 
 console.log(typeof (99.9));
-
+//number
 console.log(typeof ("false"));
-
+//string
 console.log(typeof (false));
-
+//boolean
 console.log(typeof ('0'));
-
+//string
 console.log(typeof (0));
-
+//number
 console.log(typeof (true));
-
+//boolean
 console.log(typeof ('true'));
+//string
 
 //A term or phrase typed into a search box?
 //string
