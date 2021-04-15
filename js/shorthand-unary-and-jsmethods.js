d12e9001ce89a58b6087d0a6de3da4ea35224cad
Mitch Hogue <mitchhogue@Mitchs-MacBook-Pro.local>
2021-04-15 16:13:03 -0500

    pushes lecture exercise

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//SHORTHAND ASSIGNMENT OPERATORS
+
+//+= / -=
+
+var name = "Mitchell";
+
+name += " " + "Hogue";
+console.log(name);
+
+var daysRemainingInCohort = 114;
+
+daysRemainingInCohort -= 4;
+
+console.log(daysRemainingInCohort);
+
+//MINI EXERCISE
+
+// console.log(3 + 1); //addition operator
+var result1 = 3;
+console.log(result1 += 1);
+// console.log(5 - 1); //subtraction operator
+var result2 =5;
+console.log(result2 -= 1);
+// console.log(4 * 1); //multiplication operator
+var result3 = 4;
+console.log(result3 *= 1);
+// console.log(12 / 3); //division operator
+var result4 = 12;
+console.log(result4 /= 3);
+// console.log(24 % 5); //modulus operator
+var result5 = 24;
+console.log(result5 %= 5);
+
+
+
