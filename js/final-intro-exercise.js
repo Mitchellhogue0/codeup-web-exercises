91ddd9343d74c3a516b94a98dd0e0b1bd2bf116b
Mitch Hogue <mitchhogue@Mitchs-MacBook-Pro.local>
2021-04-15 17:59:02 -0500

    pushes final exercise

---
+++ js/final-intro-exercise.js
@@ -0,0 +1,145 @@
+"use strict";
+
+var a = 1;
+var b = a++;
+var c = ++a;
+// what is the value of a, b, and c?
+console.log(a, b, c);
+
+var d = "hello";
+var e = false;
+
+d++;
+e++;
+console.log(d,e);
+
+var perplexed; // perplexed is undefined (no value is assigned)
+perplexed + 2;
+console.log(perplexed);
+
+var price = 2.7;
+price.toFixed(2);
+console.log(price);
+
+// var price = "2.7";
+// price.toFixed(2);
+// console.log(price);
+
+
+isNaN(0)
+console.log(isNaN(0));
+isNaN(1)
+console.log(isNaN(1));
+isNaN("")
+console.log(isNaN(""));
+isNaN("string")
+console.log(isNaN("string"));
+isNaN("0")
+console.log(isNaN("0"));
+isNaN("1")
+console.log(isNaN("1"));
+isNaN("3.145")
+console.log(isNaN("3.145"));
+isNaN(Number.MAX_VALUE)
+console.log(isNaN(Number.MAX_VALUE));
+isNaN(Infinity)
+console.log(isNaN(Infinity));
+isNaN("true")
+console.log(isNaN("true"));
+isNaN(true)
+console.log(isNaN(true));
+isNaN("false")
+console.log(isNaN("false"));
+isNaN(false)
+console.log(isNaN(false));
+// to illustrate why the isNaN() function is needed:
+NaN == NaN
+console.log(NaN == NaN);
+
+
+!true
+console.log(!true);
+
+!false
+console.log(!false);
+
+!!true
+console.log(!!true);
+
+!!false
+console.log(!!false);
+
+!!0
+console.log(!!0);
+
+!!-0
+console.log(!!-0);
+
+!!1
+console.log(!!1);
+
+!!-1
+console.log(!!-1);
+
+!!0.1
+console.log(!!0.1);
+
+!!"hello"
+console.log(!!"hello");
+
+!!""
+console.log(!!"");
+
+!!''
+console.log(!!'');
+
+!!"false"
+console.log(!!"false");
+
+!!"0"
+console.log(!!"0");
+
+
+var sample = "Hello Codeup";
+
+console.log(sample.length);
+
+console.log(sample.toUpperCase());
+console.log(sample.toLowerCase());
+
+sample += " " + "Students";
+console.log(sample);
+
+var sample2 = sample.replace(/Students/g, "Class");
+console.log(sample2);
+
+console.log(sample2.indexOf("C"));
+
+var sample3 = sample2.substring(6, 12);
+console.log(sample3);
+
+
+var theLittleMermaid = 3;
+var brotherBear = 5;
+var hercules = 1;
+var pricePerDay = 3;
+
+console.log((theLittleMermaid*pricePerDay) + (brotherBear*pricePerDay) + (hercules*pricePerDay));
+
+var google = 400;
+var amazon = 380;
+var facebook = 350;
+var week = 7;
+
+console.log((google*week) + (amazon*week) + (facebook*week));
+
+
+
+var username = 'codeup';
+var password = 'notastrongpassword';
+
+var check = (password > 5) && (password !== username) && (username < 20);
+console.log(check);
+
+
+
--- js/shorthand-unary-and-jsmethods.js
+++ js/shorthand-unary-and-jsmethods.js
@@ -33,5 +33,41 @@ console.log(result4 /= 3);
 var result5 = 24;
 console.log(result5 %= 5);
 
+//BUILT IN JS METHODS
+
+//toString();
+
+var num = 5 * 5;
+
+console.log(typeof num.toString());
+
+//toFix();
+
+var pi = 3.1415926535;
+
+console.log(pi.toFixed(6));
+
+//Number();
+
+console.log(Number("1234"));
+
+//isNaN() checks to see if value is not-a-number
+
+console.log(isNaN("Mitchell"));
+
+//parseInt();
+
+console.log(parseInt("123412 1232"));
+
+//.length;
+
+console.log("hello world".length);
+
+console.log("hello world".indexOf("w"));
+
+
+
+
+
 
 
