1a7f3468877b34c82e10914ed45d8ec15ad9d7bd
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-04-22 17:36:22 -0500

    commits updates to exercise

--- js/while.js
+++ js/while.js
@@ -9,25 +9,22 @@
     }
     console.log(lineBreak);
 
+
     // This is how you get a random number between 50 and 100
     var allCones = Math.floor(Math.random() * 50) + 50;
     console.log(`I have ${allCones} to sell! come buy!`)
     do{
        var conesBought = Math.floor(Math.random() * 5) + 1;
-       var conesLeft = allCones - conesBought;
-        allCones -= conesBought;
-       if (allCones !== 0){
+        console.log(`I want to buy ${conesBought} cones`)
+        if (conesBought <= allCones){
+            allCones -= conesBought;
             console.log(`${conesBought} sold, thank you!`)
+        } else{
+            console.log(`I'm sorry I only have ${allCones} left :(`)
         }
-        if (conesBought >= 6){
-            console.log(`I cannot sell you ${conesBought}...` )
-        }
-        console.log(`I only have ${conesLeft} cones left!`)
+        console.log(`I only have ${allCones} cones left!`)
         console.log(lineBreak);
-        if (conesBought > conesLeft){
-            console.log(`I'm sorry I only have ${conesLeft} left :(`)
-        }
-    } while(allCones >= 0);
+    } while(allCones > 0);
 
     console.log("YAY! I sold them all!")
 
