bdac81cbed93c9432686a5230889f03fabe4ec0e
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-04-26 17:18:47 -0500

    commits array manipulation exercises

---
+++ js/planets-string.js
@@ -0,0 +1,21 @@
+(function() {
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray;
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+
+    planetsArray= planetsString.split('|')
+
+
+
+console.log(planetsArray);
+
+
+})();
