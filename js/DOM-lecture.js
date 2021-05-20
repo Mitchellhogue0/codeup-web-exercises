2ffaada04b4aeabcfd06341f9fba64c030825d73
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-05-20 11:26:57 -0500

    commits DOM mini exercises

--- js/DOM-lecture.js
+++ js/DOM-lecture.js
@@ -82,3 +82,80 @@ for(var i = 0; i < yellowDiv.length; i++){
 // // change inner contents
 let yellowPar = document.getElementById('change-txt');
 console.log(yellowPar.innerText);
+
+//creating an element
+
+// 1.create an element
+let newEl = document.createElement("p");
+
+//2. change its innerHTML
+newEl.innerText = "Hello this is my new paragraph"
+
+// inner HTML
+let pizzaForm = document.getElementsByClassName("pizza")
+pizzaForm[0].innerHTML = " ";
+
+console.log(lineBreak)
+// // MINI EXERCISE // //
+let newEl2 = document.createElement("ul");
+let newEl3 = document.createElement("li")
+let newEl4 = document.createElement("li")
+let newEl5 = document.createElement("li")
+newEl2.appendChild(newEl3);
+newEl2.appendChild(newEl4);
+newEl2.appendChild(newEl5);
+newEl3.innerHTML = ("one")
+newEl4.innerHTML = ("two")
+newEl5.innerHTML = ("three")
+console.log(newEl2)
+let body = document.querySelector('body');
+body.appendChild(newEl2);
+newEl4.setAttribute('id', 'two')
+
+// // EXERCISE END // //
+console.log(lineBreak)
+
+
+//3.append it as a child to a parent element
+pizzaForm[0].appendChild(newEl);
+console.log(newEl)
+
+
+// query selector --> node, returns first thing you're looking for (searches from top to bottom
+//querySelectorAll --> you dont have to
+// getElementsByClassName --> HTMLCollection
+
+// // REMOVING CHILDREN 
+// 1. select our element
+let twoLi = document.querySelector('#two') //li with id of two
+console.log(twoLi);
+newEl2.removeChild(twoLi);
+
+
+console.log(lineBreak)
+// // MINI EXERCISE 2 // //
+// TODO: change the text inside of 1 of the paragraphs to be a short paragraph about you
+// TODO: log the second paragraph element from the list of all
+//  paragraphs
+// TODO: create a new ul element and 5 new li elements. Each of your li's should have some text inside of it
+// TODO: replace the last div's paragraph with your new ul. make sure the ul has all 5 li's inside of it
+// TODO: remove the last li
+
+var firstP = document.getElementById('firstP')
+firstP.innerText = "This paragraph is now about me, I enjoy this way more than I do CSS :)"
+let p = document.getElementsByTagName("p")
+console.log(p[1]);
+newUl = document.createElement("ul")
+newUl.innerHTML = "<li>one</li><li>two</li><li>three</li><li>four</li><li>five</li>"
+firstP.appendChild(newUl)
+
+// // EXERCISE END // //
+console.log(lineBreak)
+
+
+
+
+
+
+
+
