f044cc0ed3fd7d7bcb9f193ca8ee66ab5f44e109
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-06-01 12:54:45 -0500

    commits jquery lecture

---
+++ js/jquery-lecture.js
@@ -0,0 +1,38 @@
+// dollar sign tapping into jquery library
+$("selector")
+// "selector" is any element in the dom you want to access. ie id,class,element, etc.
+
+// example object
+let school = {
+    name: "Lubbock High",
+    location: "Lubbock",
+    Seniors: 523
+}
+
+// using jquery to select by tag
+$("h1")
+// using jquery to select by id
+$("#paragraph")
+// using jquery to select by class
+$(".paragraphs")
+
+// // JQUERY THINGS
+// example: hide
+// $("h2").hide()
+
+// // MINI EXERCISE
+$("#all-caps").hide();
+$("span").hide();
+$(".show-me").show();
+// // EXERCISE END
+
+// // SELECTING CHILDREN ELEMENTS AND CSS
+$("ul").children().css("color", "blue");
+
+// // ADDING CLASSES
+$("h2").addClass("border");
+// $("h2").removeClass()
+
+$(".pink").css("background-color", "pink");
+$(".orange").css("background-color", "orange");
+$("document").ready($(".orange").hide());
