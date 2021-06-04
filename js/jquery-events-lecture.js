e20449ead173b6f0a31662d7a674447cfa839a24
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-06-04 15:59:39 -0500

    commits jquery exercises

--- js/jquery-events-lecture.js
+++ js/jquery-events-lecture.js
@@ -14,33 +14,55 @@
     addClickEvents(jqElements, backgroundReset, backgroundBlue);
     addClickEvents($(".card"), "", "red");
     //make sure the DOM is loaded first
-    document.addEventListener("DOMContentLoaded", function () {
-        addListeners();
-    });
+    // document.addEventListener("DOMContentLoaded", function () {
+    //     // addListeners();
+    // });
 
     //parent method for adding all listeners
-    function addListeners() {
-        //get the elements to add listener and change image
-        let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
-        addEvents(cardContainers, 'assets/jazz-music-rubber-duck.jpg', 'mouseover');
-        addEvents(cardContainers, 'assets/question.png', 'mouseout');
+    // function addListeners() {
+    //     //get the elements to add listener and change image
+    //     let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
+    //     addEvents(cardContainers, './img/jazz-music-rubber-duck.jpg', 'mouseover');
+    //     addEvents(cardContainers, './img/jazz-music-rubber-duck.jpg', 'mouseout');
+    // }
+    //
+    // //add mouseover/mouseout events to all selected elements
+    // function addEvents(cardContainers, imgPath, listenerType) {
+    //     cardContainers.forEach((cc) => {
+    //         let card = cc;
+    //         //create listener which will call function change the image
+    //         let listener = function (event) {
+    //             let cardImg = card.querySelector(".card .card-img-top");
+    //             changeImage(cardImg, imgPath);
+    //         };
+    //         cc.addEventListener(listenerType, listener);
+    //     });
+    // }
+    //
+    // //actual function to swap images
+    // function changeImage(card, imgPath) {
+    //     card.setAttribute('src', imgPath);
+    // }
+    let card = $(".col-md-3")
+
+    function isHovered() {
+        $(".card-img-top", this).attr({
+            src: 'img/ole-rick.gif'
+        })
     }
 
-    //add mouseover/mouseout events to all selected elements
-    function addEvents(cardContainers, imgPath, listenerType) {
-        cardContainers.forEach((cc) => {
-            let card = cc;
-            //create listener which will call function change the image
-            let listener = function (event) {
-                let cardImg = card.querySelector(".card .card-img-top");
-                changeImage(cardImg, imgPath);
-            };
-            cc.addEventListener(listenerType, listener);
-        });
+    function notHovered() {
+        $(".card-img-top", this).attr({
+            src: "img/question.png"
+        })
     }
 
-    //actual function to swap images
-    function changeImage(card, imgPath) {
-        card.setAttribute('src', imgPath);
+    function beginHover() {
+        card.hover(isHovered, notHovered)
     }
+
+
+beginHover();
+
+
 })();
